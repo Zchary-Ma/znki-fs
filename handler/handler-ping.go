e33763e8f3bf13b0cd982942d2743f779138eec5
Zchary-Ma <Zcharyma@gmail.com>
2021-12-09 22:14:30 +0800

    udpate multi proto file

---
+++ handler/handler-ping.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+    "net/http"
+)
+
+func PingHandler(w http.ResponseWriter, r *http.Request)  {
+    w.Write([]byte("pong"))
+}
--- main.go
+++ main.go
@@ -1,5 +1,17 @@
 package main
 
-func main() {
+import (
+	"github.com/gorilla/mux"
+	"github.com/znkisoft/znki-fs/handler"
+	"log"
+	"net/http"
+)
 
+func main() {
+	// new router with mux
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler.HomeHandler)
+	r.HandleFunc("/ping", handler.PingHandler)
+
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
