055666ccaec57183dc6028de6d09212b127f168f
Zchary-Ma <Zcharyma@gmail.com>
2021-12-11 22:59:10 +0800

    chore

--- main.go
+++ main.go
@@ -1,10 +1,16 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gorilla/mux"
 	"github.com/znkisoft/znki-fs/handler"
 	"log"
 	"net/http"
+)
+
+var (
+	host string = "127.0.0.1"
+	port string = "8080"
 )
 
 func main() {
@@ -12,6 +18,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", handler.HomeHandler)
 	r.HandleFunc("/ping", handler.PingHandler)
+	r.HandleFunc("/user", handler.UserHandler)
 
-	log.Fatal(http.ListenAndServe(":8080", r))
+	fmt.Printf("server is running on %s:%s\n", host, port)
+	log.Fatal(http.ListenAndServe(fmt.Sprintf("%s:%s", host, port), r))
 }
